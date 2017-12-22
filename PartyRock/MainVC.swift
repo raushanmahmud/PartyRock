9585d19803f82237bc78dff41c51054a53a01283
studentas <studentas@VDUIF-503-04.local>
2017-12-22 11:47:12 +0200

    tableView plus another VC added

---
+++ PartyRock/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by studentas on 22/12/2017.
+//  Copyright © 2017 studentas. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
